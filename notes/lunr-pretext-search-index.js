0e43429d7f8d403b349bfa8f62362a8c9e29d183
ehendryx <ehendryx@users.noreply.github.com>
2025-06-30 21:09:01 +0000

    Latest build deployed.

---
+++ notes/lunr-pretext-search-index.js
@@ -0,0 +1,131 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "notes-week-01",
+  "level": "1",
+  "url": "notes-week-01.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 1",
+  "body": " Week 1  This week we covered the following topics:  "
+},
+{
+  "id": "notes-week-02",
+  "level": "1",
+  "url": "notes-week-02.html",
+  "type": "Section",
+  "number": "",
+  "title": "Week 2",
+  "body": " Week 2  This week we covered the following topics in the second week.  "
+},
+{
+  "id": "course_intro_notes",
+  "level": "1",
+  "url": "course_intro_notes.html",
+  "type": "Worksheet",
+  "number": "",
+  "title": "Brief Course Introduction",
+  "body": " Brief Course Introduction      Where have you seen math applied in other courses or even daily life?      Using mathematical theory, notation, methods, and tools to answer practical questions is a form of .    One important skill to succeed in this class and beyond is abstraction :     the realization that the letter in an equation might represent the physical size of a cell, the cost of a sweater, the size of a population, the age of a company, etc.    the understanding that although we are interested in a specific application problem, we have to carefully apply the rules of mathematics to achieve a solution---we solve an equation the same way whether or not it is purely a mathematical problem or represents a real-world problem    the recognition that many visibly different applied problems might be linked by the same mathematical description, and that what we learn from one system\/situation could teach us about another---even though we might not recognize any direct real-world similarities between the two systems      "
+},
+{
+  "id": "course_intro_notes-2-1",
+  "level": "2",
+  "url": "course_intro_notes.html#course_intro_notes-2-1",
+  "type": "Worksheet Exercise",
+  "number": "1",
+  "title": "",
+  "body": "  Where have you seen math applied in other courses or even daily life?   "
+},
+{
+  "id": "course_intro_notes-2-2",
+  "level": "2",
+  "url": "course_intro_notes.html#course_intro_notes-2-2",
+  "type": "Worksheet Exercise",
+  "number": "2",
+  "title": "",
+  "body": "  Using mathematical theory, notation, methods, and tools to answer practical questions is a form of .   "
+},
+{
+  "id": "course_intro_notes-2-3",
+  "level": "2",
+  "url": "course_intro_notes.html#course_intro_notes-2-3",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "abstraction "
+},
+{
+  "id": "linear-models",
+  "level": "1",
+  "url": "linear-models.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 1.1: Linear Models",
+  "body": " Section 1.1: Linear Models  A model describes a relationship between variables. This relationship can be represented in a variety of ways:     with a table of values    with a graph    with an equation      Ex: A taxi out of Dulles Airport in Washington, DC, charges a traveler an initial fee of $2.00, plus $1.50 for each mile traveled.     Complete the table of values showing the charge, , for a trip of miles.                      0  5  10  15  20  25                         Graph the relationship.      Axes for airport example   This is a grid for graphing      Axes for taxi example.   An 8 by 12 grid with axis origin at lower left for plotting taxi data                 Write an equation for the charge, , in terms of the number of miles traveled, .    What is the charge for a trip to Mount Vernon, 35 miles from the airport? Illustrate the answer on your graph.    If a ride to the National Institutes of Health (NIH) costs $42.50, how far is it from the airport to the NIH? Illustrate the answer on your graph.     The  of a graph are the points where the graph crosses the axes.     The  is the point found by setting  and solving for y.    The  is the point found by setting  and solving for x.      Ex: Find the intercepts of the graph in the airport example above. What do these intercepts represent?   Graphing a Line Using the “Intercept Method”:      Find the - and -intercepts.    Plot the intercepts.    Choose a value of and find a third point on the line to make it a bit easier to make the line straight.    Draw a line through the points.     In the airport example, the model was of the form  Models of this form are called  .  Linear equations can also be written in what is sometimes called general form:    Ex: Five pounds of body fat is equivalent to 16,000 calories. Carol can burn 600 calories per hour bicycling and 400 calories per hour swimming.     Write an equation in general form that relates the number of hours, , of cycling and the number of hours, , of swimming Carol needs to perform to burn 16,000 calories.    Find the intercepts and use them to sketch the graph of the linear equation.      What do the intercepts tell us about Carol's exercise program?    Is the graph you drew in part (2) increasing or decreasing ?      Ex: Frank plants a dozen corn seedlings, each 6 inches tall. With plenty of water and sunlight they will grow approximately 2 inches per day.     Complete the table of values for the height, , of the seedlings after days.                     0  5  10  15  20  25                       Write an equation for the height of the seedlings in terms of the number of days since they were planted.    Find the intercepts of the graph.    Graph the equation.    What do the intercepts tell us about the problem?     "
+},
+{
+  "id": "figure-tikz-airport",
+  "level": "2",
+  "url": "linear-models.html#figure-tikz-airport",
+  "type": "Figure",
+  "number": "1",
+  "title": "",
+  "body": " Axes for airport example   This is a grid for graphing    "
+},
+{
+  "id": "taxi-axes1",
+  "level": "2",
+  "url": "linear-models.html#taxi-axes1",
+  "type": "Figure",
+  "number": "2",
+  "title": "",
+  "body": " Axes for taxi example.   An 8 by 12 grid with axis origin at lower left for plotting taxi data           "
+},
+{
+  "id": "exponential_models",
+  "level": "1",
+  "url": "exponential_models.html",
+  "type": "Section",
+  "number": "",
+  "title": "Section 4.5: Exponential Models",
+  "body": " Section 4.5: Exponential Models  Suppose we want to fit the exponential function to two known data points. To do so, we can use the “ratio method”:     Plug the coordinates of each point into the exponential function to write two equations in terms of and .    Divide one equation by the other to eliminate and solve for .    Substitute into either equation from Step 1 and solve for .      Ex: Find an exponential function that has the values and .   Ex: Find a formula for the exponential function shown below.   Graph of exponential function         Ex: In 2005, the population of Egypt was 74 million and was growing by 2% per year.     If it continues to grow at the same rate, how long will it take the population of Egypt to double?    Given your answer to the first part, use the doubling time to write the population’s growth law.      Ex: At its current rate of growth, the population of the U.S. will double in 115.87 years. What is the annual percent growth rate of the population?   Ex: Alcohol is eliminated from the body at a rate of 15%per hour. What is the half-life of alcohol in the body?    An  is a sequence of equal payments or deposits made at equal time intervals.       If you make payments per year for years into an annuity that pays interest rate compounded times per year, the  future value  , , of the annuity is where each payment is dollars.    If you wish to receive payments per year for years from a fund that earns interest rate compounded times per year, the  present value  , , of the annuity must be where each payment is dollars.      Ex: You want to finance $25,000 to purchase a new car, and your financing institution charges an annual interest rate of 2.7%, compounded monthly. Using the formula for present value of an annuity, how large will your monthly payment be to pay off the loan in 5 years? In 6 years? Note: Making a sequence of equal payments on a loan (plus interest) is called amortizing the loan.   "
+},
+{
+  "id": "exponential_models-6",
+  "level": "2",
+  "url": "exponential_models.html#exponential_models-6",
+  "type": "Figure",
+  "number": "3",
+  "title": "",
+  "body": " Graph of exponential function     "
+},
+{
+  "id": "exponential_models-15",
+  "level": "2",
+  "url": "exponential_models.html#exponential_models-15",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": " future value   present value  "
+},
+{
+  "id": "exponential_models-16",
+  "level": "2",
+  "url": "exponential_models.html#exponential_models-16",
+  "type": "Paragraph (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "amortizing "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+  this.metadataWhitelist = ['position']
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
